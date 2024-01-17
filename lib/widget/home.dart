174a212b0da427fba37e3a375da98fcdba20663e
Nursyafiah Zahari <137259614+SyafiahZahari@users.noreply.github.com>
2024-01-17 08:03:35 +0800

    commit

--- lib/widget/home.dart
+++ lib/widget/home.dart
@@ -1,37 +1,81 @@
 import 'package:flutter/material.dart';
-//import 'login.dart';
-//import 'signup.dart';
 
-
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   final String username;
 
   HomeScreen({required this.username});
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentPageIndex = 0;
+
+  NavigationDestinationLabelBehavior labelBehavior =
+      NavigationDestinationLabelBehavior.alwaysShow;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome, $username!'),
+        title: Text('Welcome, ${widget.username}!'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome to the HomeScreen, $username!',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Add your desired functionality here
-              },
-              child: Text('Logout'),
-            ),
-          ],
-        ),
+      bottomNavigationBar: NavigationBar(
+        labelBehavior: labelBehavior,
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.favorite),
+            label: 'Favourite',
+          ),
+          NavigationDestination(
+             icon: Icon(Icons.history),
+            label: 'History',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
       ),
+
     );
+  }
+
+  Widget _buildPage(int index) {
+    switch (index) {
+      case 0:
+        // Home Page
+        return Center(
+          child: Text('Home Page for ${widget.username}'),
+        );
+      case 1:
+        // Favourite Page
+        return Center(
+          child: Text('Favourite Page for ${widget.username}'),
+        );
+      case 2:
+        // History Page
+        return Center(
+          child: Text('History Page for ${widget.username}'),
+        );
+      case 3:
+        // Profile Page
+        return Center(
+          child: Text('Profile Page for ${widget.username}'),
+        );
+      default:
+        return Container();
+    }
   }
 }
